2be5347823b6ce2a5d991a641b39d53c7df3c6e9
DanielR1 <activatingredstone@gmail.com>
2019-02-24 11:30:44 -0800

    figure out binary

--- src/_01_Bit_Shifting/BitShifter.java
+++ src/_01_Bit_Shifting/BitShifter.java
@@ -4,14 +4,43 @@ public class BitShifter {
 	public static void main(String[] args) {
 		// 1. Jot down the value of num in binary.
 		int num = 4;
-		int numShifted = num << 0;
-		
+		int numShifted = num << 1;
+		numShifted = 43;
 		// 2. Print the value of numShifted, and convert that value to binary.
-		
-		// 3. Compare the two binary values. Can you figure out what the << operator is for?
-		
+		System.out.println(numShifted);
+		int x = 1;
+		while (numShifted >= x) {
+			x = x * 2;
+		}
+		x = x / 2;
+		System.out.println(x);
+		int y = (int) (Math.log(x) / Math.log(2));
+		int binary = 0;
+		int[] a = new int[y + 1];
+		for (int i = y; i > -1; i--) {
+			a[i] = (int) Math.pow(2, i);
+		}
+		int temp = 0;
+		for (int i = y; i > -1; i--) {
+			if (numShifted >= x) {
+				binary += Math.pow(10, y);
+				temp = x;
+				x = (int) (x + x / Math.pow(2, y - i + 1));
+				// System.out.println(x);
+			} else {
+				x = temp;
+				// System.out.println(x);
+				x = (int) (x + x / Math.pow(2, y - i + 1));
+			}
+
+		}
+		System.out.println(binary);
+		// System.out.println(binary);
+		// 3. Compare the two binary values. Can you figure out what the << operator is
+		// for?
+
 		// 4. Try shifting num 3 places.
-		
-		// FYI: Binary values can be shifted to the right as well using the >> operator.	
+
+		// FYI: Binary values can be shifted to the right as well using the >> operator.
 	}
 }
