334e456a84cf0cd746e3f8cc1f6f0c108856b10f
League Student <league@iMac-9.attlocal.net>
2019-03-10 11:31:31 -0700

    do binary
    new method

--- src/_01_Bit_Shifting/BitShifter.java
+++ src/_01_Bit_Shifting/BitShifter.java
@@ -4,8 +4,9 @@ public class BitShifter {
 	public static void main(String[] args) {
 		// 1. Jot down the value of num in binary.
 		int num = 4;
-		int numShifted = num << 1;
-		numShifted = 43;
+		int numShifted = num << 2;
+		numShifted=50;
+		//numShifted = 43;
 		// 2. Print the value of numShifted, and convert that value to binary.
 		System.out.println(numShifted);
 		int x = 1;
@@ -17,19 +18,27 @@ public class BitShifter {
 		int y = (int) (Math.log(x) / Math.log(2));
 		int binary = 0;
 		int[] a = new int[y + 1];
+		int[] s = new int[y+1];
 		for (int i = y; i > -1; i--) {
 			a[i] = (int) Math.pow(2, i);
+		
+		}
+		
+		for (int i = 0; i <y+1; i++) {
+			s[i]=sumArray(a,i+1);
+			System.out.println(s[i]);
 		}
 		int temp = 0;
 		for (int i = y; i > -1; i--) {
 			if (numShifted >= x) {
-				binary += Math.pow(10, y);
+				binary += Math.pow(10, i);
 				temp = x;
 				x = (int) (x + x / Math.pow(2, y - i + 1));
 				// System.out.println(x);
+				// System.out.println(binary);
 			} else {
 				x = temp;
-				// System.out.println(x);
+				// System.out.println(x+"f");
 				x = (int) (x + x / Math.pow(2, y - i + 1));
 			}
 
@@ -42,5 +51,14 @@ public class BitShifter {
 		// 4. Try shifting num 3 places.
 
 		// FYI: Binary values can be shifted to the right as well using the >> operator.
+	}
+
+	private static int sumArray(int[] a, int i) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		for (int j = 0; j < i; j++) {
+		sum+=a[j];	
+		}
+		return sum;
 	}
 }
--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -14,10 +14,55 @@ public class BinaryPrinter {
 	}
 	
 	public void printIntBinary(int i) {
+		int x = 1;
+		while (i >= x) {
+			x = x * 2;
+		}
+		x = x / 2;
+		System.out.println(x);
+		String b = "";
+		while(i!=0) {
+			if(i>=x) {
+				b+="1";
+				i=i-x;
+				x=x/2;
+		}
+			else {
+				b+="0";
+				x=x/2;
+				
+			}
+		}
+			System.out.println(b);
+		//System.out.println(x);
+		int y = (int) (Math.log(x) / Math.log(2));
+		int binary = 0;
+//		int[] a = new int[y + 1];
+//		for (int j = y; j > -1; j--) {
+//			a[j] = (int) Math.pow(2, j);
+//		}
+//		int temp = 0;
+//		for (int j = y; j > -1; j--) {
+//			if (i >= x) {
+//				binary += Math.pow(10, j);
+//				temp = x;
+//				x = (int) (x + x / Math.pow(2, y - j + 1));
+//				// System.out.println(x);
+//			} else {
+//				x = temp;
+//				// System.out.println(x);
+//				x = (int) (x + x / Math.pow(2, y - j + 1));
+//			}
+		//System.out.println(Integer.toBinaryString(i));
 		
-	}
+		}
+	
 	
 	public void printLongBinary(long l) {
 		
+	}
+	public static void main(String[] args) {
+		BinaryPrinter b = new BinaryPrinter();
+		b.printIntBinary(256);
 	}
 }
