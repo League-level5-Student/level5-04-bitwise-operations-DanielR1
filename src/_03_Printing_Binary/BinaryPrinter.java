ba50032e95d5ef38b83758238233e32a1d180dda
League Student <league@iMac-9.attlocal.net>
2019-03-17 11:28:25 -0700

    Fix Base 64

--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -6,11 +6,68 @@ public class BinaryPrinter {
 	//Create a main method to test your methods.
 	
 	public void printByteBinary(byte b) {
+		int i = (int)b;
+		int x = 1;
+		while (i >= x) {
+			x = x * 2;
+		}
+		int y = (int) (Math.log(x) / Math.log(2));
+		x = x / 2;
+	//	System.out.println(x);
+		String c = "";
+		
+		while(i!=0) {
+			if(i>=x) {
+				c+="1";
+				i=i-x;
+				x=x/2;
+		}
+			else {
+				c+="0";
+				x=x/2;
+				
+			}
+		}
+		
+		while(c.length()!=y) {
+			c+="0";
+		}
+			System.out.println(c);
+
+
 		
 	}
 	
 	public void printShortBinary(short s) {
+		int i = (int)s;
+		int x = 1;
+		while (i >= x) {
+			x = x * 2;
+		}
+		int y = (int) (Math.log(x) / Math.log(2));
+		x = x / 2;
+	//	System.out.println(x);
+		String c = "";
 		
+		while(i!=0) {
+			if(i>=x) {
+				c+="1";
+				i=i-x;
+				x=x/2;
+		}
+			else {
+				c+="0";
+				x=x/2;
+				
+			}
+		}
+		
+		while(c.length()!=y) {
+			c+="0";
+		}
+			System.out.println(c);
+
+
 	}
 	
 	public void printIntBinary(int i) {
@@ -18,9 +75,11 @@ public class BinaryPrinter {
 		while (i >= x) {
 			x = x * 2;
 		}
+		int y = (int) (Math.log(x) / Math.log(2));
 		x = x / 2;
-		System.out.println(x);
+	//	System.out.println(x);
 		String b = "";
+		
 		while(i!=0) {
 			if(i>=x) {
 				b+="1";
@@ -33,36 +92,53 @@ public class BinaryPrinter {
 				
 			}
 		}
+		
+		while(b.length()!=y) {
+			b+="0";
+		}
 			System.out.println(b);
-		//System.out.println(x);
-		int y = (int) (Math.log(x) / Math.log(2));
-		int binary = 0;
-//		int[] a = new int[y + 1];
-//		for (int j = y; j > -1; j--) {
-//			a[j] = (int) Math.pow(2, j);
-//		}
-//		int temp = 0;
-//		for (int j = y; j > -1; j--) {
-//			if (i >= x) {
-//				binary += Math.pow(10, j);
-//				temp = x;
-//				x = (int) (x + x / Math.pow(2, y - j + 1));
-//				// System.out.println(x);
-//			} else {
-//				x = temp;
-//				// System.out.println(x);
-//				x = (int) (x + x / Math.pow(2, y - j + 1));
-//			}
-		//System.out.println(Integer.toBinaryString(i));
+
 		
 		}
 	
 	
 	public void printLongBinary(long l) {
+		int i= (int)l;
+		int x = 1;
+		while (i >= x) {
+			x = x * 2;
+		}
+		int y = (int) (Math.log(x) / Math.log(2));
+		x = x / 2;
+	//	System.out.println(x);
+		String b = "";
 		
+		while(i!=0) {
+			if(i>=x) {
+				b+="1";
+				i=i-x;
+				x=x/2;
+		}
+			else {
+				b+="0";
+				x=x/2;
+				
+			}
+		}
+		
+		while(b.length()!=y) {
+			b+="0";
+		}
+			System.out.println(b);
+
+
 	}
 	public static void main(String[] args) {
 		BinaryPrinter b = new BinaryPrinter();
-		b.printIntBinary(256);
+		b.printIntBinary(157);
+		b.printLongBinary(34);
+		b.printByteBinary((byte)6);
+		b.printShortBinary((short)134);
+	
 	}
 }
--- src/_04_Base64_Decoder/Base64Decoder.java
+++ src/_04_Base64_Decoder/Base64Decoder.java
@@ -37,19 +37,113 @@ public class Base64Decoder {
 	//1. Complete this method so that it returns the the element in
 	//   the base64Chars array that corresponds to the passed in char.
 	public static byte convertBase64Char(char c){
-		return 0;
+		int x=0;
+		for (int i = 0; i < base64Chars.length; i++) {
+			if(base64Chars[i]==c) {
+				x=i;
+			}
+		}
+		return (byte)x;
 	}
 	
 	//2. Complete this method so that it will take in a string that is 4 
 	//   characters long and return an array of 3 bytes (24 bits). The byte 
 	//   array should be the binary value of the encoded characters.
 	public static byte[] convert4CharsTo24Bits(String s){
-		return null;
+		String cb="";
+		char [] stringChars = s.toCharArray();
+		for (int i = 0; i < stringChars.length; i++) {
+			int x=indexOfCharArray(base64Chars, stringChars[i]);
+			cb+=returnIntBinary(x);
+		}
+			byte[] r =new byte[3];
+			for (int i = 0; i < 3;i++) {
+				r[i]=(byte)binaryToInteger(cb.substring(8*i, 8*i+8));
+			}
+			
+		
+		
+		return r;
 	}
 	
 	//3. Complete this method so that it takes in a string of any length
 	//   and returns the full byte array of the decoded base64 characters.
 	public static byte[] base64StringToByteArray(String file) {
-		return null;
+		String cb="";
+		char [] stringChars = file.toCharArray();
+		for (int i = 0; i < stringChars.length; i++) {
+			int x=indexOfCharArray(base64Chars, stringChars[i]);
+			cb+=returnIntBinary(x);
+		}
+			byte[] r =new byte[stringChars.length*3/4];
+			for (int i = 0; i < r.length;i++) {
+				r[i]=(byte)binaryToInteger(cb.substring(8*i, 8*i+8));
+			}
+			
+		
+		
+		return r;
 	}
+	public static int indexOfCharArray(char[] c, char index) {
+		int x=0;
+		for (int i = 0; i < c.length; i++) {
+			if(c[i]==index) {
+				x=i;
+			}
+			
+		}
+		return x;
+	}
+	public static String returnIntBinary(int i) {
+		int x = 1;
+		String b = "";
+		while (i >= x) {
+			x = x * 2;
+		}
+		int y = (int) (Math.log(x) / Math.log(2));
+		int ex=6-y;
+		for (int j = 0; j < ex; j++) {
+			b+="0";
+			//System.out.println("1");
+		}
+		x = x / 2;
+	//	System.out.println(x);
+		
+		
+		while(i!=0) {
+			if(i>=x) {
+				b+="1";
+				i=i-x;
+				x=x/2;
+		}
+			else {
+				b+="0";
+				x=x/2;
+				
+			}
+		}
+		
+		while(b.length()<y) {
+			b+="0";
+			System.out.println(b);
+		}
+			return b;
+
+		
+		}
+	public static int binaryToInteger(String binary) {
+	    char[] numbers = binary.toCharArray();
+	    int result = 0;
+//	    for(int i=numbers.length - 1; i>=0; i--) {
+//	        if(numbers[i]=='1')
+//	            result += Math.pow(2, (numbers.length-i - 1));
+//	    }
+	    for (int i = 0; i < numbers.length; i++) {
+			if(numbers[i]==1) {
+				result+=Math.pow(2, numbers.length-i-1);
+			}
+		}
+	    return result;
+	}
+	
 }
